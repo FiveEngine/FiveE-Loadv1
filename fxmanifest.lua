7c4028b111ad7b580de720ae24c85e034e625fb6
Five Engine <batudevs1316@icloud.com>
2024-08-26 14:26:51 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'baTu / FiveEngine'
+description 'https://discord.gg/fiven'
+lua54 'yes'
+
+-- loadscreen_manual_shutdown "yes" -- Ekranda Mouse Görmek İstiyosan Bunu Satırı Açabilirsin Bunu Açıcaksan CursorType Kapatabilirsin
+
+files {
+    '*.html', 
+    'assets/**/*.*', 
+    'assets/**/**/*.*'
+}
+loadscreen 'index.html'
+
